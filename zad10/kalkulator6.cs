f04b39bafa868dd26790beec61e3dacb2d5647d4
ds65y <vitalliy2337@gmail.com>
2024-12-06 19:53:02 +0100

    Update kalkulator6.cs

--- zad10/kalkulator6.cs
+++ zad10/kalkulator6.cs
@@ -2,9 +2,9 @@ using System;
 
 class Program
 {
-    static char[] znaki;
-    static int[] mznak; 
-    static int licznik = 0;
+    static char[] znaki; 
+    static int[] mznak;   
+    static int licznik = 0; 
 
     static void Main()
     {
@@ -12,8 +12,9 @@ class Program
         string dzialanie = Console.ReadLine();
         Znak(dzialanie);
         double wynik = 0d;
-        double[] liczby = new double[licznik + 1]; 
-        int pocz, koniec;
+        double[] liczby = new double[licznik + 1];
+
+       int pocz, koniec;
         for (int i1 = 0; i1 < licznik + 1; i1++){
             if (i1 == 0) pocz = 0;
             else pocz = mznak[i1 - 1] + 1;
@@ -26,8 +27,7 @@ class Program
 
         wynik = liczby[0];
 
-        for (int i2 = 0; i2 < licznik; i2++)
-        {
+        for (int i2 = 0; i2 < licznik; i2++){
             if (znaki[i2] == '+')
                 wynik = Dodawanie(wynik, liczby[i2 + 1]);
             else if (znaki[i2] == '-')
@@ -37,24 +37,23 @@ class Program
             else if (znaki[i2] == '/')
                 wynik = Dzielenie(wynik, liczby[i2 + 1]);
         }
+
         Console.WriteLine("Wynik: " + wynik);
     }
-    static void Znak(string dzialanie)
-    {
+
+    static void Znak(string dzialanie){
         znaki = new char[dzialanie.Length];
         mznak = new int[dzialanie.Length];
         licznik = 0;
-
-        for (int i = 0; i < dzialanie.Length; i++)
-        {
-        if (dzialanie[i] == '+' || dzialanie[i] == '-' || dzialanie[i] == '*' || dzialanie[i] == '/'){
-        znaki[licznik] = dzialanie[i];
-        mznak[licznik] = i;
-        licznik++;
+        
+        for (int i = 0; i < dzialanie.Length; i++){
+            if (dzialanie[i] == '+' || dzialanie[i] == '-' || dzialanie[i] == '*' || dzialanie[i] == '/') {
+             znaki[licznik] = dzialanie[i];
+             mznak[licznik] = i;
+             licznik++;
+            }
         }
     }
-    }
-
     static double Dodawanie(double a, double b)    {return a + b;}
     static double Odejmowanie(double a, double b)    {return a - b;}
     static double Mnorzenie(double a, double b)    {return a * b;}
